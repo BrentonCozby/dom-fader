7b75744e911dccfb2604773d4baf42e30e136e67
Brenton Cozby <brenton.cozby@gmail.com>
2018-01-03 22:16:42 -0500

    make them immutable

--- src/dom-fader.js
+++ src/dom-fader.js
@@ -2,9 +2,11 @@
 // so that fadeIn will go back to the original display value
 let CSSvalues = {}
 
-function fade(element, _speed, direction, easing) {
+function fade(element, fadeSpeed, direction, easing) {
     // abort fading if is already fading in or out
-    if(element.dataset.fading) return false
+    if (element.dataset.fading) {
+        return false
+    }
 
     element.dataset.fading = true
 
@@ -12,10 +14,10 @@ function fade(element, _speed, direction, easing) {
     const savedValues = CSSvalues[element.dataset.domFaderId]
     const thisDisplay = window.getComputedStyle(element).getPropertyValue('display')
     const thisOpacity = window.getComputedStyle(element).getPropertyValue('opacity')
-    const speed = (_speed) ? _speed : (_speed === 0) ? 0 : 300
+    const speed = (fadeSpeed) ? fadeSpeed : (fadeSpeed === 0) ? 0 : 300
 
-    if(!element.dataset.domFaderId) {
-        const id = Math.random()
+    if (!element.dataset.domFaderId) {
+        const id = (Date.now() * Math.random()).toFixed(0)
         element.dataset.domFaderId = id
         CSSvalues[id] = {
             display: thisDisplay === 'none' ? 'block' : thisDisplay,
@@ -24,27 +26,33 @@ function fade(element, _speed, direction, easing) {
     }
 
     // add/remove the styles that will animate the element
-    if(direction === 'in') {
+    if (direction === 'in') {
         s.opacity = '0'
         s.display = (savedValues) ? savedValues.display : 'block'
         s.transition = `opacity ${speed}ms ${easing || ''}`
         setTimeout(() => s.opacity = (savedValues) ? savedValues.opacity : '1', 10)
     }
-    if(direction === 'out') {
+
+    if (direction === 'out') {
         s.transition = `opacity ${speed}ms ${easing || ''}`
         s.opacity = '0'
     }
 
     // remove temp styles, add DOM-fader-hidden class, and return the element
-    let done = new Promise(function(resolve, reject) {
-        setTimeout(function() {
+    let done = new Promise((resolve, reject) => {
+        setTimeout(() => {
             element.removeAttribute('style')
             element.removeAttribute('data-fading')
-            if(direction === 'in') {
+
+            if (direction === 'in') {
                 element.classList.remove('DOM-fader-hidden')
                 s.display = (savedValues) ? savedValues.display : 'block'
             }
-            if(direction === 'out') element.classList.add('DOM-fader-hidden')
+
+            if (direction === 'out') {
+                element.classList.add('DOM-fader-hidden')
+            }
+
             resolve(element)
         }, speed)
     })
@@ -62,21 +70,35 @@ function fade(element, _speed, direction, easing) {
     `
     document.head.appendChild(sheet)
 
-    Object.prototype.fadeIn = function(_speed, easing) {
-        return fade(this, _speed, 'in', easing)
-    }
-
-    Object.prototype.fadeOut = function(_speed, easing) {
-        return fade(this, _speed, 'out', easing)
-    }
-
-    Object.prototype.fadeToggle = function(_speed, easing) {
-        if(this.classList.contains('DOM-fader-hidden')) {
-            return fade(this, _speed, 'in', easing)
+    Object.defineProperty(Object.prototype, 'fadeIn', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (fadeSpeed, easing) {
+            return fade(this, fadeSpeed, 'in', easing)
         }
-        else {
-            return fade(this, _speed, 'out', easing)
-        }
+    })
 
-    }
+    Object.defineProperty(Object.prototype, 'fadeOut', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (fadeSpeed, easing) {
+            return fade(this, fadeSpeed, 'out', easing)
+        }
+    })
+
+    Object.defineProperty(Object.prototype, 'fadeToggle', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (fadeSpeed, easing) {
+            if (this.classList.contains('DOM-fader-hidden')) {
+                return fade(this, fadeSpeed, 'in', easing)
+            } else {
+                return fade(this, fadeSpeed, 'out', easing)
+            }
+
+        }
+    })
 })()
